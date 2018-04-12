5888e929bae095a2f44141dc43e4f7d24e10a56f
Fernando Reis <fmreis@gmail.com>
2018-04-13 00:30:40 +0100

    functional proggraming tests

--- src/test/field/annotation/FieldAnnotationExample.java
+++ src/test/field/annotation/FieldAnnotationExample.java
@@ -3,22 +3,24 @@ package test.field.annotation;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class FieldAnnotationExample {
 
     public static void main(String[] args) {
 
-    TheClass theClass = new TheClass();
-    setField(theClass, "RECO_STRING_FIELD", "AAA");
-    setField(theClass, "RECO_DECIMAL_FIELD", new BigDecimal(123.10000000));
+        TheClass theClass = new TheClass();
+        setField(theClass, "RECO_STRING_FIELD", "AAA");
+        setField(theClass, "RECO_DECIMAL_FIELD", new BigDecimal(123.10000000));
 
-    System.out.println(theClass.myStringField);
-    System.out.println(theClass.myDecimalField);
+        System.out.println(theClass.myStringField);
+        System.out.println(theClass.myDecimalField);
 
     }
 
-    public static void setField(TheClass theClass, String fieldName, Object value){
+    public static void setField(TheClass theClass, String fieldName, Object value) {
         Field[] fields = TheClass.class.getFields();
+
         for (Field field: fields) {
             Annotation[] annotations = field.getDeclaredAnnotations();
             for(Annotation annotation: annotations){
@@ -32,6 +34,26 @@ public class FieldAnnotationExample {
                 }
             }
         }
+
+//        Arrays.stream(fields).forEach(field -> {
+//            Annotation[] annotations = field.getDeclaredAnnotations();
+//            Arrays.stream(annotations).forEach(annotation -> {
+//                if (((MyAnnotation) annotation).name().equals(fieldName))
+//                    try {
+//                        field.set(theClass, value);
+//                    } catch (IllegalAccessException e) {
+//                        e.printStackTrace();
+//                    }
+//            });
+//        });
+
+//        Arrays.stream(fields)
+//                .forEach(field -> {
+//                        Arrays.stream(field.getDeclaredAnnotations()).
+//                                filter(annotation -> ((MyAnnotation) annotation).name().equals(fieldName))
+//                                .anyMatch(field.set(theClass, value));
+//        });
+
     }
 }
 
